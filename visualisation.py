b53839a993ca76b4354f3655d67cf745d1715ca4
vicdlp <128626573+vicdlp@users.noreply.github.com>
2023-03-22 18:58:14 +0100

    Update visualisation.py

--- visualisation.py
+++ visualisation.py
@@ -19,8 +19,12 @@ T = pd.to_datetime(pd.Series(df['Date'] + ' ' + df['Time'], dtype = "datetime64"
 Bx = pd.Series(df["Bx"], dtype = 'float') # champ magnétique au format float
 By = pd.Series(df["By"], dtype = 'float')
 Bz = pd.Series(df["Bz"], dtype = 'float')
-
 B_norm = np.sqrt(Bx**2 + By**2 + Bz**2) # calcul de la norme de B
+
+Bxc = Bx - np.mean(Bx) # champ magnétique centré sur sa valeur moyenne
+Byc = By - np.mean(By)
+Bzc = Bz - np.mean(Bz)
+Bc = B_norm - np.mean(B_norm)
 
 # Affichage des données du log
 
@@ -54,4 +58,42 @@ plt.ylabel("Champ magnétique (μT)")
 plt.plot(T, B_norm, label = "Norme")
 plt.legend()
 plt.grid()
+plt.show()
+
+# Calcul du spectre du signal
+
+X1 = fft(Bxc)
+X2 = fft(Byc)
+X3 = fft(Bzc)
+N = len(T)
+n = np.arange(N)
+# get the sampling rate
+sr = 8 # Hz
+tau = N/sr
+freq = n/tau 
+
+# Get the one-sided specturm
+n_oneside = N//2
+# get the one side frequency
+f_oneside = freq[:n_oneside] # 
+
+plt.figure()
+plt.plot(f_oneside, np.abs(X1)[:n_oneside], label = "Bx")
+plt.xlabel('Freq (Hz)')
+plt.legend()
+plt.ylabel('FFT Amplitude |X(freq)|')
+plt.show()
+
+plt.figure()
+plt.plot(f_oneside, np.abs(X2)[:n_oneside], label = "By")
+plt.xlabel('Freq (Hz)')
+plt.legend()
+plt.ylabel('FFT Amplitude |X(freq)|')
+plt.show()
+
+plt.figure()
+plt.plot(f_oneside, np.abs(X3)[:n_oneside], label = 'Bz')
+plt.xlabel('Freq (Hz)')
+plt.legend()
+plt.ylabel('FFT Amplitude |X(freq)|')
 plt.show()
